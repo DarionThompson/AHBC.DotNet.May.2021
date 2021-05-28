d220062c6003b3d1dcd950e1cd78eab4d8dcb030
Darion Thompson <darionthompson2@gmail.com>
2021-05-27 20:13:16 -0400

    added for and foreach loop example

--- AHBC_DOTNET_MAY_2021/Loops/Program.cs
+++ AHBC_DOTNET_MAY_2021/Loops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Loops
 {
@@ -10,65 +11,99 @@ namespace Loops
 
             //string response = Console.ReadLine();
 
-            if (true)
-            {
-                //Then execute this line of code
-            }
-            else
-            {
-                //Then execute this line of code
-            }
-
-            int switchNumber = 5;
-
-            switch (switchNumber)
-            {
-                case 1: Console.WriteLine("case one");
-                    break;
-
-                case 5:
-                    Console.WriteLine("Case 5");
-                    break;
-
-                default:
-                    break;
-            }
-
-            //Explicit casting
-            double number = 22.50;
-            int secondNumber = (int)number;
-
-            //implicit Casting
-            int thirdNumber = 3;
-            double num = thirdNumber;
-
-            while (num <= 5)
-            {
-                Console.WriteLine("I Just looped bro");
-                num++;
-            }
-            //Example of an infinite loop
-            //while (false)
+            //if (true)
             //{
-            //    Console.WriteLine("I will never end bro");
+            //    //Then execute this line of code
+            //}
+            //else
+            //{
+            //    //Then execute this line of code
             //}
 
-            int secondControlNumber = 0;
-            do
-            {
-                Console.WriteLine("I will always execute at least once");
-                secondControlNumber++;
-            } while (secondControlNumber <= 5);
+            //int switchNumber = 5;
 
-            //I want to keep looping until the user tells me no (N)
-            Console.WriteLine("Hey Bro Can you tell me (Y) yes or (N) no ?");
-            string userInput = "";
-            do
-            {
-                Console.WriteLine("Do you want me to keep looping");
-                userInput = Console.ReadLine();
+            //switch (switchNumber)
+            //{
+            //    case 1: Console.WriteLine("case one");
+            //        break;
 
-            } while (userInput != "N");
+            //    case 5:
+            //        Console.WriteLine("Case 5");
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+
+            ////Explicit casting
+            //double number = 22.50;
+            //int secondNumber = (int)number;
+
+            ////implicit Casting
+            //int thirdNumber = 3;
+            //double num = thirdNumber;
+
+            //while (num <= 5)
+            //{
+            //    Console.WriteLine("I Just looped bro");
+            //    num++;
+            //}
+            ////Example of an infinite loop
+            ////while (false)
+            ////{
+            ////    Console.WriteLine("I will never end bro");
+            ////}
+
+            //int secondControlNumber = 0;
+            //do
+            //{
+            //    Console.WriteLine("I will always execute at least once");
+            //    secondControlNumber++;
+            //} while (secondControlNumber <= 5);
+
+            ////I want to keep looping until the user tells me no (N)
+            //Console.WriteLine("Hey Bro Can you tell me (Y) yes or (N) no ?");
+            //string userInput = "";
+            //do
+            //{
+            //    Console.WriteLine("Do you want me to keep looping");
+            //    userInput = Console.ReadLine();
+
+            //} while (userInput != "N");
+
+            //for-loop
+            
+
+            //for (int j = 10; j > 0; j--)
+            //{
+            //    //3
+            //    Console.WriteLine(j);
+            //}
+
+            var names = new List<string>
+            {
+                "Robert",
+                "Bobby Bax",
+                "The Bax man"
+            };
+            var nameE = "Robert";
+            
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            int[] array = new int[]
+            {
+              //0,1,2,3,4
+                1,2
+            };
+            //1             2               4
+            for (int i = 0; i < 3; i++)
+            {
+                //3
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
