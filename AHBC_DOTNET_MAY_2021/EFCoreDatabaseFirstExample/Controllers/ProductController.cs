afd4cc015ec6ea7d6b7b4fd4d6f1d306456ecdb3
Darion Thompson <darion@stepquest.com>
2021-08-26 16:53:39 -0400

    db first ef core

---
+++ AHBC_DOTNET_MAY_2021/EFCoreDatabaseFirstExample/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreDatabaseFirstExample.Models;
+using EFCoreDatabaseFirstExample.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreDatabaseFirstExample.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Here 
+            var model = await _productRepository.GetProductAsync();
+            return View(model);
+        }
+    }
+}
---
+++ AHBC_DOTNET_MAY_2021/EFCoreDatabaseFirstExample/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using EFCoreDatabaseFirstExample.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreDatabaseFirstExample.Repository
+{
+    public class ProductRepository
+    {
+        private readonly NorthwindContext _northwindContext;
+
+        public ProductRepository(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+        //C.R.U.D
+        //Read
+        public async Task<List<Product>> GetProductAsync()
+        {
+            var products = await _northwindContext.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
